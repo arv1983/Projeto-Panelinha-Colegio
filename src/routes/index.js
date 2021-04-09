0e5c90fbb4a5cdc5aff68743c77ad84968b6b297
FIRST_NAME LAST_NAME <gianine1508@gmail.com>
2021-04-09 09:19:54 -0300

    feat: att pageRegLog e pasta pagehome e paageProfile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,13 @@
 import { Switch, Route } from "react-router-dom";
-import Login from "../components/Login";
 import UpProfileComp from "../components/UpProfileComp";
 import UpProfileDev from "../components/UpProfileDev";
-import PageRegister from "../pages";
+import PageLogReg from "../pages/PageLogReg";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <PageRegister/>
-        
+        <PageLogReg/>
       </Route>
       <Route exact path="/users">
         <UpProfileComp />
