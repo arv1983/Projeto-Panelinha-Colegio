3f1947861c8658412ce8b27c5106482ebffa8ec8
Renan  Sampaio <renansampaio0401@yahoo.com>
2021-04-12 17:03:29 -0400

    feat: (loggout)

--- src/components/Navegation/index.js
+++ src/components/Navegation/index.js
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { User } from "../../providers/UserProvider";
+import Logout from "../Loggout";
 import { Head } from "./style";
 
 const Navegation = () => {
-    return(
-        <Head>
-            <Link to="/home">Principal</Link>
-            <Link to="/users/comp">Company</Link>
-            <Link to="/users/dev">Usuario</Link>
-            <Link to="/pesquisa">Pesquisa</Link>
-        </Head>
-    )
-}
+  const { loggedUser } = User();
+
+  return (
+    <Head>
+      <Link to="/home">Principal</Link>
+      {loggedUser.type === "pj" && <Link to="/users/comp">Empresa</Link>}
+      {loggedUser.type === "pf" && <Link to="/users/dev">Usuario</Link>}
+      <Link to="/pesquisa">Pesquisa</Link>
+      <Logout />
+    </Head>
+  );
+};
 
 export default Navegation;
