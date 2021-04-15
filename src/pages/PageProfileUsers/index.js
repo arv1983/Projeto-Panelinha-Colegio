e1fe7e2c710bd7a13048c55380ac11c77aa83151
FIRST_NAME LAST_NAME <gianine1508@gmail.com>
2021-04-15 13:54:36 -0300

    featch: finalizacao da tela user

--- src/pages/PageProfileUsers/index.js
+++ src/pages/PageProfileUsers/index.js
@@ -1,7 +1,5 @@
 import AlteraHead from "../../components/AlterHead";
-import Navegation from "../../components/Navegation";
 import UpProfileDev from "../../components/UpProfileDev";
-
 import {DivComp, DivAvatar, DivProfile} from '../../stylesGlobal'
 
 const PageProfileUsers = ()=>{    
@@ -16,9 +14,6 @@ const PageProfileUsers = ()=>{
                     <UpProfileDev/>
                 </DivComp>
             </DivProfile>
-            <div  style={{ border:"1px solid white", width: "99%", height: "auto", margin: "30px auto" , textAlign: "center"}}>
-                <h1>Tecnologias</h1>
-            </div>
         </div>
         )
     }
