4b66d6c0d761641d8f19b31722a9b40634bf7303
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-04-17 14:15:39 -0300

    feat pagehomedev

---
+++ src/pages/PageHomeDev/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { COLORS } from "../../stylesGlobal";
+export const Principa = styled.div`
+  height: auto;
+  background-color: ${COLORS.cinza};
+  color: ${COLORS.roxo};
+  width: 270px;
+  margin: 10px auto;
+  padding: 10px;
+  border-radius: 20px;
+  .img {
+    background-color: white;
+    padding: 10px;
+    border-radius: 20px;
+    margin-top: 5px;
+    img {
+      width: 50px;
+      margin: 5px;
+    }
+  }
+`;
