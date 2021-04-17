d6dfc366dfe175d26e3a509069cf8da07dca380e
Andre Marques <simple_single@protonmail.com>
2021-04-16 21:08:37 -0400

    fix: Bug Cypress test

--- cypress/integration/capstonetest/login.spec.js
+++ cypress/integration/capstonetest/login.spec.js
@@ -11,19 +11,26 @@ cy.get(".sc-bdvvaa").contains("Login")
 cy.get(":nth-child(1) > .sc-gsDJrp").type("capstone2@gmail.com");
 cy.get(":nth-child(2) > .sc-gsDJrp").type("123456");
 cy.get(".sc-bdvvaa").click();
-cy.contains("Atualizar Perfil Dev")
+cy.wait(5000);
+cy.contains("Atualizar Perfil Dev");
 cy.get(":nth-child(1) > a").click();
-cy.contains("DESCRIÇÃO");
+cy.wait(5000);
+cy.contains("Voce tem:");
+cy.get(":nth-child(2) > a").click();
+cy.wait(5000);
+cy.contains("Atualizar Perfil Dev");
+cy.get(":nth-child(1) > .sc-dkPtyc").type("Renato");
 cy.get(":nth-child(3) > a").click();
-cy.contains("Dev");
-cy.get("#simple-tab-0").click();
+cy.wait(5000);
+cy.get(":nth-child(3) > a").click();
 cy.contains("Pesquise um Dev");
-cy.get("#simple-tab-1 > .MuiTab-wrapper").click();
-cy.contains("Pesquisar uma Empresa:");
+cy.get("#simple-tab-1").click();
 cy.get("#simple-tab-2 > .MuiTab-wrapper").click();
-cy.contains("Pesquisar Vagas");
 cy.get(":nth-child(4) > a").click();
-cy.contains("Bem - Vindo");
+cy.wait(2000);
+cy.contains("Recrutamento");
+
+
 
 })
 
